5ca410f3e0d2c5e0b6e96bfc21466e2df4a3ed44
lbugiola <nicodonosocastex@hotmail.com>
2023-04-03 11:07:35 -0300

    cambio en conexion base

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,15 +1,33 @@
+const mongoose = require("mongoose");
+
 import { connect, connection } from "mongoose";
 
 const conn = {
   isConnected: false,
 };
 
-export async function dbConnect() {
-  const db = await connect(process.env.MONGODB_URI|| process.env.MONGODB_LOCAL);
+export const dbConnect = async () => {
+  const uri =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URI
+      : process.env.MONGODB_LOCAL;
+
+  const db = await connect(uri);
+
   console.log(db.connection.db.databaseName);
-  conn.isConnected = db.connections[0].readyState;
-}
+
+  const [{ readyState }] = db.connections;
+  conn.isConnected = readyState;
+};
+
+const handleError = (error) => {
+  console.error("Mongodb Errro:", error.message);
+};
 
 connection.on("connected", () => console.log("Mongodb connected to db"));
 
-connection.on("error", (err) => console.error("Mongodb Errro:", err.message));
+connection.removeListener("error", handleError);
+
+mongoose.set("strictQuery", false);
+
+export { connection };
